e3008784cc475c2cee52667529fa99091962ba88
Kanishk Saini <kanishksaini1604@gmail.com>
2025-09-20 01:31:09 -0400

    updated resume

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -82,7 +82,7 @@ const Navbar = () => {
             </a>
             <Button className="ml-4" asChild>
               <a 
-                href="https://drive.google.com/file/d/1ka3eFKO2fbyXCQURpTO4ZMjABxxgvhZt/view" 
+                href="https://drive.google.com/file/d/1FYL8dxtKX-fcPvkD-61wVSt8VVsLKsrY/view?usp=sharing" 
                 target="_blank" 
                 rel="noopener noreferrer"
               >
