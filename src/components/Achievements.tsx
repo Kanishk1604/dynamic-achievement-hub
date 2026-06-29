16fc8ea9588168cd7adbc2476785c75a79d7c5d5
Kanishk1604 <kanishksaini1604@gmail.com>
2026-06-29 00:36:09 -0400

    updated awards section

--- src/components/Achievements.tsx
+++ src/components/Achievements.tsx
@@ -4,6 +4,12 @@ import { Award, Star, Medal } from "lucide-react";
 
 const achievements = [
   {
+    title: "World Record Literary Honor Certificate",
+    description: "Recognized by the Golden Book of World Records for contributing to the AAINA Heritage World Record Project through literary participation, promoting Hindi literature and cultural heritage globally (2026).",
+    icon: <Medal className="h-10 w-10 text-primary" />,
+    link: "https://drive.google.com/file/d/1dYzl4fbGKGKuSEYfOwikH6YsNI-OJrib/view?usp=sharing"
+  },
+  {
     title: "President's Honour Roll",
     description: "Recognized for academic excellence.",
     icon: <Star className="h-10 w-10 text-yellow-500" />,
